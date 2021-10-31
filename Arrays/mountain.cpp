023046eb7052973da916bd7b33d06899896d0b0c
Pranshu Srivastava <iampranshu24@gmail.com>
2021-10-31 23:46:24 +0530

    Coding minutes

--- Arrays/mountain.cpp
+++ Arrays/mountain.cpp
@@ -1,27 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int highest_mountain(vector<int> a){
+int highest_mountain(vector<int> a)
+{
     int n = a.size();
     int largest = 0;
-    for(int i=1;i<n-1;i)
+    for (int i = 1; i < n - 1;)
     {
-        if(ca[i]>a[i-1] and a[i]>a[i+1])
+        if (a[i] > a[i - 1] and a[i] > a[i + 1])
         {
             int cnt = 1;
-            while(j>=1 and a[j]>a[j-1])
+            int j = i;
+            while (j >= 1 and a[j] > a[j - 1])
             {
                 j--;
                 cnt++;
             }
-            while(i<n-1 and a[i]>a[i+1])
+            while (i < n - 1 and a[i] > a[i + 1])
             {
                 i++;
                 cnt++;
             }
+            largest = max(largest, cnt);
+            cout << a[i] << endl;
         }
-        else 
-        i++;
-
+        else
+            i++;
     }
     return largest;
 }
@@ -35,7 +38,7 @@ int main()
     ios_base::sync_with_stdio(NULL);
     cin.tie(NULL);
 
-    vector<int> array{5,6,1,2,3,4,5,4,2,3,0,1,2,3,-4};
-    cout<<highest_mountain(array);
+    vector<int> array{5, 6, 1, 2, 3, 4, 5, 4, 2, 3, 0, 1, 2, 3, -4};
+    cout << highest_mountain(array);
     return 0;
 }

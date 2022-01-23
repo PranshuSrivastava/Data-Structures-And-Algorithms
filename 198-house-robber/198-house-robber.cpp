96011c9698889a26327ef66522d5f50e1f235b24
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-01-23 17:01:21 +0000

    Time: 0 ms (100.00%), Space: 7.8 MB (21.70%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, vector<int>& sol, int i)
-    {
-        if(i>=nums.size())
-            return 0;
+    int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)
-            return nums[0];
-        if(n==2)
-            return max(nums[1],nums[0]);
-        if(sol[i]==-1)
-                                                                                               sol[i]=max(helper(nums,sol,i+2)+nums[i],helper(nums,sol,i+1));
-        return sol[i];
-    }
-    int rob(vector<int>& nums) {
+            return nums[0];        
         vector<int> sol(nums.size()+1,-1);
-        cout<<sol[0];
-        return helper(nums,sol,0);
-        
+        sol[0]=nums[0];
+        sol[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            sol[i]=max(sol[i-1],nums[i]+sol[i-2]);
+        }
+        return sol[n-1];
     }
 };

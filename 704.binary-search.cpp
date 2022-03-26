a6b0a4d915a5f6c59220736cd5eacb3d1ed19686
PranshuSrivastava <iampranshu24@gmail.com>
2022-03-26 17:17:43 +0530

    leetcode

--- 704.binary-search.cpp
+++ 704.binary-search.cpp
@@ -7,14 +7,23 @@
 // @lc code=start
 #include <bits/stdc++.h>
 #define ll long long int
-#define pb push_back
-#define yes cout << "YES" << '\n';
-#define no cout << "NO" << '\n';
-#define vi vector<int>
-#define mi unordered_map<int, int>
-#define nline cout << '\n';
+#define pb pop_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define mi unordered_map<int,int>
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define vi vector<int>;
+#define vl vector<ll>;
+#define vpii vector<pii>;
+#define vs vector<string>;
+#define vvs vector<vs>;
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 #define sz(x) (int)x.size()
 #define lt(x) (int)x.length()
+#define vvi vector<vi>;
+#define all(x) x.begin,x.end()
 using namespace std;
 class Solution
 {

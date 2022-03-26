54de0c76cc580591b9c1b4ac3de5cba64b73fe69
PranshuSrivastava <iampranshu24@gmail.com>
2022-03-26 12:26:07 +0530

    leetcode

--- 704.binary-search.cpp
+++ 704.binary-search.cpp
@@ -5,11 +5,34 @@
  */
 
 // @lc code=start
-class Solution {
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define vi vector<int>
+#define mi unordered_map<int, int>
+#define nline cout << '\n';
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+using namespace std;
+class Solution
+{
 public:
-    int search(vector<int>& nums, int target) {
-        
+    int search(vector<int> &nums, int target)
+    {
+        int ub = sz(nums) - 1, lb = 0, mid;
+        while (lb <= ub)
+        {
+            mid = (lb + ub) / 2;
+            if(nums[mid] == target)
+            return mid;
+            else if(nums[mid]<target)
+            lb=mid+1;
+            else
+            ub=mid-1;
+        }
+        return -1;
     }
 };
 // @lc code=end
-

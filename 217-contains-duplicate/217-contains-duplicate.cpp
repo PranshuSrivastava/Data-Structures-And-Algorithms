c693963dfb85aab27ff77bf5bca6e1396144ee8d
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-03-26 17:17:40 +0530

    Time: 174 ms (17.58%), Space: 54.9 MB (5.86%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb pop_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define mi unordered_map<int,int>
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define vi vector<int>;
+#define vl vector<ll>;
+#define vpii vector<pii>;
+#define vs vector<string>;
+#define vvs vector<vs>;
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define vvi vector<vi>;
+using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s(all(nums));
+        if(sz(s) == sz(nums) )
+        return 0;
+        return 1;
+
+    }
+};

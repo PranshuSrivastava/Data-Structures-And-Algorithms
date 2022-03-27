46362c028080f92c158c964c1774b00e66c8db1a
PranshuSrivastava <iampranshu24@gmail.com>
2022-03-27 21:07:51 +0530

    MarinandPhotoshoot

---
+++ codeforces/MarinandPhotoshoot.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+#define mod 1e9+7
+typedef std :: unordered_map<int, int> mi ;
+typedef std :: vector<std :: string> vs;
+typedef std :: vector<vs> vvs;
+typedef std :: pair<int, int>  pii;
+typedef std :: pair<ll, ll>    pl;
+typedef std :: vector<int>     vi;
+typedef std :: vector<ll>      vl;
+typedef std :: vector<pii>     vpii;
+typedef std :: vector<pl>      vpl;
+typedef std :: vector<vi>      vvi;
+typedef std :: vector<vl>      vvl;
+
+
+using namespace std;
+
+void solve() {
+	int n;
+	string s;
+	cin >> n >> s;
+	int cnt1 = count(all(s), '1');
+	cout << cnt1 << '\n';
+
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}

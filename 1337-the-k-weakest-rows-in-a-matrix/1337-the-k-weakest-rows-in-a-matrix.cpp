70889e6eaa28698d1fbced1daba61dec0cf3bef7
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-03-27 12:41:32 +0530

    Time: 40 ms (5.48%), Space: 10.7 MB (19.79%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int,int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+#define mod 1e9+7
+using namespace std;
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        set<pii> s;
+        for(int i =0; i<sz(mat); ++i){
+            int cnt = count(all(mat[i]),1);
+            s.insert({cnt,i});
+        }
+        vi ans;
+        for(auto i : s){
+            if(!k)
+            break;
+            ans.pb(i.S);
+            k--;
+        }
+        return ans;
+
+    }
+};

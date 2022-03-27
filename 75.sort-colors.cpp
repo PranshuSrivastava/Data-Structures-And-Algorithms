cd8ce87e5417702a27ae274b1cee441fab9601c5
PranshuSrivastava <iampranshu24@gmail.com>
2022-03-27 13:49:26 +0530

    leetcode

---
+++ 75.sort-colors.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=75 lang=cpp
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout << '\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int, int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1e9 + 7
+using namespace std;
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int lo = 0, mid = 0, high = sz(nums) - 1;
+        while (mid <= high)
+        {
+            switch (nums[mid])
+            {
+            case 0:
+                swap(nums[lo++], nums[mid++]);break;
+            case 1:
+                mid++;break;
+            case 2: swap(nums[mid],nums[high--]);
+            }
+        }
+    }
+};
+// @lc code=end

bc0bd3c0854a4f5aff8df3ad4923ac95200e2885
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-03-29 13:01:40 +0530

    Time: 140 ms (67.55%), Space: 61.3 MB (61.36%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int,int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+#define mod 1e9+7
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // Floyd Cycle Detection.
+        int fast = nums[0], slow = nums[0];
+        do{
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }while(fast!=slow);
+        slow = nums[0];
+        while(fast!= slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return fast;
+
+    }
+};

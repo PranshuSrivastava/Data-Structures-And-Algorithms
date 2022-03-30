9f549d936fde15e887b51713cdc3fde6134814a1
PranshuSrivastava <iampranshu24@gmail.com>
2022-03-30 18:50:41 +0530

    leetcode

---
+++ 435.non-overlapping-intervals.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=435 lang=cpp
+ *
+ * [435] Non-overlapping Intervals
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int,int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+#define mod 1e9+7
+using namespace std;
+    bool comp(vi &a, vi &b){
+        return a[1]<b[1];
+    }
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(all(intervals), comp);
+        int ans = -1;
+        vi prev = intervals[0];
+        for(vi i : intervals){
+            if(prev[1]>i[0])
+            ++ans;
+            else
+            prev = i;
+        }
+        return ans;
+    }
+};
+// @lc code=end
+

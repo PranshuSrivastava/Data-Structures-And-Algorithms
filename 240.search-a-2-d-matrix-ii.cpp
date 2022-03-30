d3e82f0da89e37f7eb8a6372c421cc42cd64ade3
PranshuSrivastava <iampranshu24@gmail.com>
2022-03-30 16:10:04 +0530

    leetcode

---
+++ 240.search-a-2-d-matrix-ii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=240 lang=cpp
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout << '\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int, int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1e9 + 7
+using namespace std;
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int m = sz(matrix), n = m ? sz(matrix[0]) : 0, r = 0, c = n - 1;
+        while (r < m and c >= 0)
+        {
+            if (matrix[r][c] == target)
+                return 1;
+            matrix[r][c] > target ? c-- : r++;
+        }
+        return 0;
+    }
+};
+// @lc code=end

a2026d58cc03e8164034ca3be0167cfc0782e901
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-04-23 11:21:09 +0530

    Time: 3 ms (83.88%), Space: 7.2 MB (29.61%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int,int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+#define mod 1e9+7
+using namespace std;
+class Solution {
+public:
+    unordered_map<string,string> shortner;
+    int counter = 0;
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        string shortUrl = "http://tinyurl.com";
+        shortner[shortUrl] = longUrl;
+        ++counter;
+        return shortUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return shortner[shortUrl];
+
+    }
+};

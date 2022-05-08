861fba83d59f91c2a2e0451345713fe05efdfcfe
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-05-08 12:17:58 +0530

    Time: 34 ms (20.83%), Space: 24.9 MB (8.81%) - LeetHub

---
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout << '\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int, int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1e9 + 7
+using namespace std;
+class Solution
+{
+public:
+    map<int,int> maxi;
+    int helper(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        int l = helper(root->left);
+        int r = helper(root->right);
+        ++maxi[l + r + root->val];
+        return l + r + root->val;
+    }
+    vector<int> findFrequentTreeSum(TreeNode *root)
+    {
+        vi freq;
+        helper(root);
+        int maxn, frequency = 0;
+        for(auto x: maxi){
+            if(x.second>=frequency)
+            {
+                frequency = x.second;
+            }
+        }
+        for(auto x: maxi){
+            if(x.second==frequency)
+            freq.pb(x.first);
+        }
+
+        return freq;
+    }
+};

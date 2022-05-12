c709f176944fa217bc8fb542b10232960499461a
pranshu <iampranshu24@gmail.com>
2022-05-13 01:12:55 +0530

    leetcode

--- 112.path-sum.cpp
+++ 112.path-sum.cpp
@@ -42,22 +42,94 @@ typedef vector<vi> vvi;
 typedef vector<vl> vvl;
 #define mod 1e9 + 7
 using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout << '\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int, int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1e9 + 7
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout << '\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int, int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+#define mod 1e9 + 7
+using namespace std;
 class Solution
 {
 public:
     bool hasPathSum(TreeNode *root, int targetSum)
     {
-        if (!root->left and !root->right and !targetSum)
+        if (!root)
+            return false;
+        if (!root->left and !root->right and !(targetSum - root->val))
         {
-            deb(root->val);
-            if (targetSum == 0)
-                return true;
-            else
-                return false;
+
+            return true;
         }
         bool left = hasPathSum(root->left, targetSum - root->val);
         bool right = hasPathSum(root->right, targetSum - root->val);
-        deb(right);
         return left || right;
     }
 };

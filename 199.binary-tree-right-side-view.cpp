d291973406673ddb93dd4a8aad0f5b60e0de7b1a
pranshu <iampranshu24@gmail.com>
2022-05-13 03:07:51 +0530

    leetcode

---
+++ 199.binary-tree-right-side-view.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=199 lang=cpp
+ *
+ * [199] Binary Tree Right Side View
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int,int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+#define mod 1e9+7
+using namespace std;
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vi ans;
+        dfs(root, ans);
+        return ans;
+    }
+    void dfs(TreeNode* root, vi& ans){
+        if(!root)
+        return ;
+
+        ans.pb(root->val);
+        dfs(root->right, ans);
+    }
+};
+// @lc code=end
+

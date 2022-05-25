5a512862558f6e79a7d7a2bce24706a7a49ccea4
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-05-26 00:19:49 +0530

    Time: 12 ms (83.29%), Space: 10.2 MB (97.89%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int,int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+#define mod 1e9+7
+using namespace std;
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = sz(nums);
+        vi dp;
+        for(int i : nums)
+        {
+            auto it = lower_bound(all(dp), i);
+            if(it == dp.end())
+            dp.pb(i);
+            else
+            *it = i;
+        }
+        return sz(dp);
+    }
+};

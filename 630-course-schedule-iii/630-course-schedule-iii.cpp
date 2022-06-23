56adec02b1c2711654b56d3dfa1bc8b488e511cf
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-06-23 15:25:07 +0530

    Time: 542 ms (19.67%), Space: 63.1 MB (35.57%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -7,17 +7,15 @@ public:
         int n = courses.size();
         sort(courses.begin(), courses.end(), helper);
         priority_queue<int> q;
-        int sum = 0, count = 0;
+        int sum = 0;
         for(auto c : courses){
             sum += c[0];
             q.push(c[0]);
-            ++count;
             if(sum > c[1]){
                 sum -= q.top();
                 q.pop();
-                --count;
             }
         }
-        return count;
+        return q.size();
     }
 };

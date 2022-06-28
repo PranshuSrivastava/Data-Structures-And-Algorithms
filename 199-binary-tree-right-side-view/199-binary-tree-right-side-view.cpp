226f41e5080774fe64e45dfdb1a5e259c3d430b3
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-06-28 17:39:19 +0530

    Time: 6 ms (48.32%), Space: 12 MB (66.72%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define deb(x) cout << #x << "=" << x << endl
+#define all(x) x.begin(), x.end()
+#define nline cout<<'\n';
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sz(x) (int)x.size()
+#define lt(x) (int)x.length()
+#define F first
+#define S second
+typedef unordered_map<int,int> mi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>    pl;
+typedef vector<int>     vi;
+typedef vector<ll>      vl;
+typedef vector<pii>     vpii;
+typedef vector<pl>      vpl;
+typedef vector<vi>      vvi;
+typedef vector<vl>      vvl;
+#define mod 1e9+7
+using namespace std;
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vi ans;
+        dfs(root, ans, 0);
+        return ans;
+    }
+    void dfs(TreeNode* root, vi& ans, int level){
+        if(!root)
+        return ;
+        if(level == ans.size())
+        ans.pb(root->val);
+        dfs(root->right, ans, level + 1);
+        dfs(root->left, ans, level + 1);
+    }
+};

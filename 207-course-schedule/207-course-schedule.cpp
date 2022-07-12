25f1c09a2f3d0031f88bc90c32df21190975141c
Pranshu Srivastava <37413698+PranshuSrivastava@users.noreply.github.com>
2022-07-12 12:12:04 +0530

    Time: 27 ms (73.93%), Space: 13.4 MB (75.21%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> vis(n);
         vector<vector<int>> adj(n);
         for(int i = 0; i < pre.size(); ++i){
-            cout<<"ok";
             adj[pre[i][0]].push_back(pre[i][1]);
         }
-        vector<int> vis(n);
         for(int i = 0; i < n; ++i){
             if(!vis[i])
-            {
-                if(helper(adj, vis, i))
+                if(dfs(adj, vis, i))
                     return 0;
-            }
         }
         return 1;
     }
-    bool helper(vector<vector<int>>& adj, vector<int>& vis, int curr){
+    bool dfs(vector<vector<int>>& adj, vector<int>& vis, int curr){
         if(vis[curr] == 2)
             return 1;
         vis[curr] = 2;
         for(int i = 0; i < adj[curr].size(); ++i){
             if(vis[adj[curr][i]] != 1)
-                if(helper(adj, vis, adj[curr][i]))
+                if(dfs(adj, vis, adj[curr][i]))
                     return 1;
         }
         vis[curr] = 1;
